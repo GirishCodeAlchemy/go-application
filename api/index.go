01966c2c41e9bef0b60ec26b2d4a91e9bdf8f06b
Girish V <143807663+GirishCodeAlchemy@users.noreply.github.com>
2024-08-09 00:55:23 -0400

    Updated the index code

--- api/index.go
+++ api/index.go
@@ -1,14 +1,29 @@
-package main
+package handler
 
 import (
 	"log"
 	"net/http"
 )
 
-func main() {
-    fs := http.FileServer(http.Dir("static"))
-    http.Handle("/", http.StripPrefix("/", fs))
+// func main() {
+//     fs := http.FileServer(http.Dir("static"))
+//     http.Handle("/", http.StripPrefix("/", fs))
 
-    log.Println("Server started on :8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+//     log.Println("Server started on :8080")
+//     log.Fatal(http.ListenAndServe(":8080", nil))
+// }
+
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+  // Serve static files if the URL path starts with "/static/"
+  if r.URL.Path == "/" {
+    // fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+//   } else if r.URL.Path == "/" || len(r.URL.Path) > 8 && r.URL.Path[:8] == "/static/" {
+    fs := http.FileServer(http.Dir("static"))
+    http.StripPrefix("/", fs).ServeHTTP(w, r)
+  } else {
+    http.NotFound(w, r)
+  }
 }
+
+
